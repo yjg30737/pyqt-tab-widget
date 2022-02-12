3937d7838c102b57d41578276f7a6bb26a4a91a7
Jung Gyu Yoon <yjg30737@gmail.com>
2022-02-12 14:27:09 +0900

    Correct the grammar in description section of setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license='MIT',
     packages=find_packages(),
     description='PyQt QTabWidget which is the most common type. '
-                'This has a lot of common feature such as close tab, close tabs to the left/right, '
+                'This has a lot of common features such as close a tab, close tabs to the left/right, '
                 'close other tabs and so on.',
     url='https://github.com/yjg30737/pyqt-common-tab-widget.git',
     install_requires=[
